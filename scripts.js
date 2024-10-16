1df3845a3fd369de5037f9d2bbb1c2239d775e0a
chloe-ui218 <chloerhinan10@gmail.com>
2024-10-16 03:19:19 -1200

    feat: implemented add recipe

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 const recipes = [
     {
-        title: "spaghetti Bolognese",
+        title: "Spaghetti Bolognese",
         ingredients: "spaghetti, Ground Beef, Tomato Sauce, Onions, Garlic, Olive Oil",
         steps: "1. Boil Pasta. 2. Cook Ground Beef. 3. Add Sauce and garlic, Onion, and Garlic. 4. Mix with pasta."
     },
@@ -15,7 +15,13 @@ const recipes = [
         title: "Vegetable Stir-fry",
         ingredients: "Broccoli, Carrots, Bell peppers, Soy sauce, Garlic, Olive Oil",
         steps: "1. Stir-fry vegetables on olive oil. 2. Add garlic and soy sauce. 3. serve with rice."
+    },
+    {
+        title: "Pancakes",
+        ingredients: "Flour, Eggs, Milk, Sugar, Baking Powder, Salt",
+        steps: "Mix ingredients in a bowl. 2. Pour butter on a hot griddle. 3. Flip pancake when bubbles form."
     }
+
 ];
 
 const displayRecipes = () => {
@@ -34,4 +40,35 @@ const displayRecipes = () => {
     })
 }
 
+const addRecipe = () => {
+    const recipeTitleInput = document.querySelector("#recipeTitle");
+    const recipeIngredientsInput = document.querySelector("#recipeIngredients");
+    const recipesInput = document.querySelector("#recipeSteps");
+
+    const recipeTitle = recipeTitleInput.value.trim();
+    const recipeIngredients = recipeIngredientsInput.value.trim();
+    const recipeSteps = recipeStepsInput.value.trim();
+
+    if (recipeTitle !== "" && recipeIngredients !== "" && recipeSteps !== ""){
+        const newRecipe = {
+            title: recipeTitle,
+            ingredients: recipeIngredients,
+            steps:recipeSteps
+        }
+        recipes.push(newRecipe);
+
+        recipeTitleInput.value = "";
+        recipeIngredientsInput = "";
+        
+
+    } else{
+        alert("Please fill out all fields");
+    }
+}
 displayRecipes();
+const addRecipeBtn = document.querySelector("#addRecipe");
+addRecipeButton.addEventListener("click, addRecipe")
+
+
+
+
