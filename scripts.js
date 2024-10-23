d195cfa2063627d94ad63537dd30d6511e85e713
chloe-ui218 <chloerhinan10@gmail.com>
2024-10-22 12:42:47 -1200

    chore: able to load data from local storage

--- scripts.js
+++ scripts.js
@@ -1,21 +1,21 @@
-const recipes = [
-    {
-        title: "Spaghetti Bolognese",
-        ingredients: "spaghetti, Ground Beef, Tomato Sauce, Onions, Garlic, Olive Oil",
-        steps: "1. Boil Pasta. 2. Cook Ground Beef. 3. Add Sauce and garlic, Onion, and Garlic. 4. Mix with pasta."
-    },
+let recipes = [
+    // {
+    //    title: "Spaghetti Bolognese",
+    //    ingredients: "spaghetti, Ground Beef, Tomato Sauce, Onions, Garlic, Olive Oil",
+    //    steps: "1. Boil Pasta. 2. Cook Ground Beef. 3. Add Sauce and garlic, Onion, and Garlic. 4. Mix with pasta."
+    //},
 
-    {
-        title: "Chicken Curry",
-        ingredients: "chicken, Curry powder, coconut milk, Onions, Garlic, Ginger",
-        steps: "1. Cook chicken. 2. Add onions, garlic, ginger. 3. Add coconut milk and curry powder. 4. simmer."
-    },
+    // {
+    //    title: "Chicken Curry",
+    //    ingredients: "chicken, Curry powder, coconut milk, Onions, Garlic, Ginger",
+    //    steps: "1. Cook chicken. 2. Add onions, garlic, ginger. 3. Add coconut milk and curry powder. 4. simmer."
+    // },
 
-    {
-        title: "Vegetable Stir-fry",
-        ingredients: "Broccoli, Carrots, Bell peppers, Soy sauce, Garlic, Olive Oil",
-        steps: "1. Stir-fry vegetables on olive oil. 2. Add garlic and soy sauce. 3. serve with rice."
-    },
+    // {
+    //    title: "Vegetable Stir-fry",
+    //    ingredients: "Broccoli, Carrots, Bell peppers, Soy sauce, Garlic, Olive Oil",
+    //    steps: "1. Stir-fry vegetables on olive oil. 2. Add garlic and soy sauce. 3. serve with rice."
+    // },
 ];
 
 const displayRecipes = () => {
@@ -122,5 +122,7 @@ displayRecipes();
 // recipeForm.addEventListener("submit", addRecipe);
 
 document.getElementById("addRecipe").addEventListener("click", addRecipe);
+
+loadRecipesFromLocalStorage();
 
 
